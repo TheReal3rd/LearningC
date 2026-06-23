8e8efd5f77b79bd15eaf46508ae0a1a277022095
³ʳᵈ <60966545+TheReal3rd@users.noreply.github.com>
2026-06-23 16:58:02 +0100

    Update PraticeMallocSorting.c
    not working work on it when i get home lol

--- Random/PraticeMallocSorting.c
+++ Random/PraticeMallocSorting.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <bool.h>
+#include <stdbool.h>
 
 int *unSortedArr;
 int *sortedArr;
@@ -19,26 +19,40 @@ int main() {
     unSortedArr = malloc(size * sizeof(int));
     printf("Unsorted array: \n");
     for(int i = 0; i != size - 1; i++) {
-        unSortedArr[i] = getRandom(0, 10);
+        unSortedArr[i] = getRandom(10, 100);
         printf("%d\n", unSortedArr[i]);
     }
     sortedArr = malloc(size * sizeof(int));
-    int lowestValue = 0;
+    int lowestValue = 99999;
     int in = 0;
+	int indexShift = 0;
     bool sorted = false;
     while(!sorted) {
         int temp = unSortedArr[in];
+		printf("temp: %d\n", temp);
         if(temp < lowestValue) {
             lowestValue = temp;
         }
-        //TODO not done
         
-        in++;
         if(in >= size) {
-            in = 0;
-        }
+			printf("Set sorted %d in %d\n", lowestValue, indexShift);
+			sortedArr[indexShift] = lowestValue;
+			lowestValue = 99999;
+			indexShift++;
+			in = indexShift;
+			if(indexShift >= size) {
+				sorted = true;
+			}
+        } else {
+			in++;
+		}
+		printf("index pos %d %d\n",in, indexShift);
     }
     
+	printf("\n\nSorted list now:\n");
+	for(int i = 0; i != size; i++) {
+		printf("%d\n", sortedArr[i]);
+	}
     
     free(unSortedArr);
     free(sortedArr);
